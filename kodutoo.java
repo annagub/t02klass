e67c2884fdc1ae07a0351ca9bb17d15384ee9630
annagubs <annagubs@greeny.cs.tlu.ee>
2017-02-08 23:39:28 +0200

    kodutoo 2

---
+++ kodutoo.java
@@ -0,0 +1,34 @@
+public class kodutoo{
+	double a, b, c;
+	public kodutoo(double kylg1, double kylg2, double kylg3){
+		if(kylg1+kylg2<kylg3) {throw new RuntimeException("Sobimatud kyljepikkused");}
+		if(kylg1+kylg3<kylg2) {throw new RuntimeException("Sobimatud kyljepikkused");}
+		if(kylg2+kylg3<kylg1) {throw new RuntimeException("Sobimatud kyljepikkused");}
+		a=kylg1;
+		b=kylg2;
+	    c=kylg3;
+		}
+		
+	public double ymberm66t(){
+		return a+b+c;
+	
+	}
+	public double pindala(){
+		double p=(a+b+c)/2;
+		return Math.round(Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+	
+	}
+	public String nurgad(){
+		double C=(Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2))/(2*a*b);
+		double A=(Math.pow(c, 2) + Math.pow(b, 2) - Math.pow(a, 2))/(2*c*b);
+		double B=(Math.pow(c, 2) + Math.pow(a, 2) - Math.pow(b, 2))/(2*c*a);
+		return "Nurk C on "+Math.round(Math.toDegrees(Math.acos(C)))+" kraadi, Nurk A on "+Math.round(Math.toDegrees(Math.acos(A)))+" kraadi, Nurk B on "+Math.round(Math.toDegrees(Math.acos(B)))+" kraadi.";
+	
+	}
+	public String k6rgus(){
+		double p=(a+b+c)/2;
+		double H=((a/2)*Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+		return "Kolmnurga k6rgus H on "+Math.round(H);
+	}
+	
+}
---
+++ kodutoo2.java
@@ -0,0 +1,16 @@
+public class kodutoo2{
+	public static void main(String [] args){
+		kodutoo k1=new kodutoo(5, 5, 6);
+		System.out.println("Ymbermoot on: "+k1.ymberm66t());
+		System.out.println("Pindala on: "+k1.pindala());
+		System.out.println(k1.nurgad());
+		System.out.println(k1.k6rgus());
+	
+	}
+/*
+Ymbermoot on: 16.0
+Pindala on: 12.0
+Nurk C on 74 kraadi, Nurk A on 53 kraadi, Nurk B on 53 kraadi.
+Kolmnurga k6rgus H on 30
+*/
+}
